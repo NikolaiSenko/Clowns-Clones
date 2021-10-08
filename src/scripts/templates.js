102aefa5e4b52d190f46908e279893cb3e1fc2a6
Senko Nikolai <kolya.senko333@gmail.com>
2021-10-08 11:38:31 +0300

    refactor: refactor htm

--- src/scripts/templates.js
+++ src/scripts/templates.js
@@ -12,13 +12,13 @@ function createCard({ Image, avatar, Description, id } = post) {
   card.id = `${id}`;
   let cardHeader = createElement("div", "card__header");
   let imgMain = createElement("img", "card__pictures");
-  imgMain.setAttribute("src", `${Image}`);
+  imgMain.src = Image;
   imgMain.setAttribute("crossorigin", "");
   let buttonCardTop = createElement("button", "card__button--top", "Сохранить");
   let buttonCardBottom = createElement("button", "card__button--bottom", "...");
   let cardFooter = createElement("div", "card__footer");
   let imgAvatars = createElement("img", "card__avatars");
-  imgAvatars.setAttribute("src", `${avatar}`);
+  imgAvatars.src = avatar;
   let descriptions = createElement("p", "card__descriptions", `${Description}`);
   cardHeader.append(imgMain, buttonCardTop, buttonCardBottom);
   cardFooter.append(imgAvatars, descriptions);
