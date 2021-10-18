471fa76f7f0a8266262cb09077425a4298dc4887
Aleksei <aleksei.brazovskii@gmail.com>
2021-10-18 20:34:45 +0300

    pas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -46,10 +46,17 @@ function onSearch(e) {
       loadCards()
         .then(randomCards)
         .then((posts) => sortCard(posts, input))
-        .then(renderCards)
+        .then((posts) => (posts.length !== 0 ? renderCards(posts) : pashalka()))
         .catch(alert);
     }
   }
+}
+
+function pashalka() {
+  const nothingFound = document.createElement("h2");
+  const main = document.getElementById("main");
+  nothingFound.innerText = "На нашем христианском сервере,ничего не найдено!!!";
+  main.append(nothingFound);
 }
 
 function onSelect(event) {
@@ -57,13 +64,10 @@ function onSelect(event) {
   const { animals, films, others } = WEBSTORAGECONFIG;
   const value = event.target.value;
   if (value === "animals") {
-    renderSelect(value);
     renderBoard(animals);
   } else if (value === "films") {
-    renderSelect(value);
     renderBoard(films);
   } else if (value === "others") {
-    renderSelect(value);
     renderBoard(others);
   }
 }
@@ -81,25 +85,6 @@ function onCard(board) {
     }
   } else if (target.className === "card__button--bottom") {
     showChoiceWindow(main, cardId);
-  }
-}
-
-function renderSelect(value) {
-  const select = document.getElementById("choice");
-  const [checked, animalsOption, filmsOption, outherOption] =
-    select.parentElement;
-  if (value === "animals") {
-    animalsOption.innerHTML = "Animals" + " &check;";
-    filmsOption.innerHTML = "Films";
-    outherOption.innerHTML = "Others";
-  } else if (value === "films") {
-    animalsOption.innerHTML = "Animals";
-    filmsOption.innerHTML = "Films" + " &check;";
-    outherOption.innerHTML = "Others";
-  } else if (value === "others") {
-    animalsOption.innerHTML = "Animals";
-    filmsOption.innerHTML = "Films";
-    outherOption.innerHTML = "Others" + " &check;";
   }
 }
 
